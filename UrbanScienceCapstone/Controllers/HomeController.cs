e6de0c26a83f6b422bad2b741d3e37010c4c6102
Will-Renius <reniuswi@msu.edu>
2017-09-10 15:11:46 -0400

    parse keywords with cognitive services api

--- UrbanScienceCapstone/Controllers/HomeController.cs
+++ UrbanScienceCapstone/Controllers/HomeController.cs
@@ -1,37 +1,99 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+//non default
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using Newtonsoft.Json;
 using UrbanScienceCapstone.Models;
+using Newtonsoft.Json.Linq;
+
+
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace UrbanScienceCapstone.Controllers
 {
+    struct Document
+    {
+        public string language;
+        public string id;
+        public string text;
+    }
+
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        const string subscriptionKey = "fc2944a69ec44b03939f48cf7a7a0ad3";
+        const string uriBase = "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases";
+
+        // GET: /<controller>/
+        //swtichted iactionresult to ation result, may want to switch back
+        public ActionResult Index()
         {
+
+            
             return View();
         }
-
-        public IActionResult About()
+        [HttpPost]
+        public ActionResult Search(Search model)
         {
-            ViewData["Message"] = "Your application description page.";
+            if (ModelState.IsValid)
+            {
+                //TODO: SubscribeUser(model.Email);
+            }
+            return RedirectToAction("Keywords", "Home", new { search = model.search });
 
-            return View();
         }
 
-        public IActionResult Contact()
+        public async Task<ActionResult> Keywords(string search)
         {
-            ViewData["Message"] = "Your contact page.";
+            List<Document> documents = new List<Document>();
+            documents.Add(new Document() { language = "en", id = "1", text = search});
 
+            List<Keywords> KeywordInfo = new List<Keywords>();
+            var client = new HttpClient();
+
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            HttpResponseMessage response;
+
+            // Compose request.
+            string body = "";
+            foreach (Document doc in documents)
+            {
+                if (!string.IsNullOrEmpty(body))
+                {
+                    body = body + ",";
+                }
+
+                body = body + "{ \"language\": \"" + doc.language + "\", \"id\":\"" + doc.id + "\",  \"text\": \"" + doc.text + "\"   }";
+            }
+
+            body = "{  \"documents\": [" + body + "] }";
+
+            // Request body
+            byte[] byteData = Encoding.UTF8.GetBytes(body);
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                response = await client.PostAsync(uriBase, content);
+            }
+
+            // Get the JSON response
+            string result = await response.Content.ReadAsStringAsync();
+            JObject result2 = JObject.Parse(result);
+            //Deserializing the response recieved from web api and storing into the Employee list 
+            RootObject testdes = new RootObject();
+
+            //testdes = JsonConvert.DeserializeObject<RootObject>(result);
+            testdes = result2.ToObject<RootObject>();
+            ViewBag.keyPhrases = testdes.documents[0].keyPhrases;
             return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
---
+++ UrbanScienceCapstone/Models/Keywords.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UrbanScienceCapstone.Models
+{
+    public class Keywords
+    {
+        public int Id { get; set; }
+        public List<string> KeyPhrases {get; set;}
+    }
+
+    public class Document
+    {
+        public List<string> keyPhrases { get; set; }
+        public string id { get; set; }
+    }
+
+    public class RootObject
+    {
+        public List<Document> documents { get; set; }
+        public List<object> errors { get; set; }
+    }
+    public class Search
+    {
+        public string search { get; set; }
+    }
+}
